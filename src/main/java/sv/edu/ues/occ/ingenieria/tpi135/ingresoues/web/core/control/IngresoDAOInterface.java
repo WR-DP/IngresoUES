f78d71f2340aa7a7829b01fcb70c977a69f88d58
sauron <dp20007@ues.edu.sv>
2026-03-05 16:03:50 -0600

    limpieza de metodos faltantes

--- src/main/java/sv/edu/ues/occ/ingenieria/tpi135/ingresoues/web/core/control/IngresoDAOInterface.java
+++ src/main/java/sv/edu/ues/occ/ingenieria/tpi135/ingresoues/web/core/control/IngresoDAOInterface.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public interface IngresoDAOInterface <T, ID>{
     void create(T obj)throws IllegalStateException, IllegalArgumentException;
-    //void delete(T obj)throws IllegalStateException, IllegalArgumentException;
+    void delete(T obj)throws IllegalStateException, IllegalArgumentException;
     T update(T obj)throws IllegalStateException, IllegalArgumentException;
-    T findById(Object id)throws IllegalStateException, IllegalArgumentException;
+    //T findById(Object id)throws IllegalStateException, IllegalArgumentException;
     List<T> findRange(int first, int max)throws IllegalStateException, IllegalArgumentException;
     //int count()throws IllegalStateException;
 
